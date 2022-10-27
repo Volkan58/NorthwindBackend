820e7a7c84170f301332514f239cc0ba416c01d6
Volkan58 <105253703+Volkan58@users.noreply.github.com>
2022-10-27 21:30:57 +0300

    Api-Auth Controller Login-Register.

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("Login")]
+        public IActionResult Login(UserForLoginDto userForLoginDto)
+        {
+            var userToLogin = _authService.Login(userForLoginDto);
+            if (!userToLogin.Success)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("Register")]
+        public IActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExitsts = _authService.UserExists(userForRegisterDto.Email);
+            if (!userExitsts.Success)
+            {
+                return BadRequest(userExitsts.Message);
+            }
+            var registerresult = _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+            var result = _authService.CreateAccessToken(registerresult.Data);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
